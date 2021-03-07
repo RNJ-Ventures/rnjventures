beb49a1cb1760c6bca918e9aacf0a37863333903
JonLuca DeCaro <jdecaro@usc.edu>
2021-03-07 02:13:24 -0500

    image fixes

--- src/components/ProfileCard/Profiles.tsx
+++ src/components/ProfileCard/Profiles.tsx
@@ -11,18 +11,18 @@ const PROFILES: Profile[] = [
     email: "jonluca@rnjventures.com",
   },
   {
-    name: "Nat Redfern",
-    bio:
-      "Nat is a third generation technologist, and product manager at Disney+ running half a dozen products. Since joining, he’s built products that have delivered $60M in recurring revenue for Disney+. Prior to Disney, Nat worked as the Chief of Staff for Contrary Capital, where he managed a team of 100+ scouts and invested in early stage startups. ",
-    picUrl: "/images/nat.jpg",
-    email: "nat@rnjventures.com",
-  },
-  {
     name: "River Mist",
     bio:
       "River is equal parts software engineer and entrepreneur. After building and selling his first company in 2016, River co-founded Steiner Financial, a real estate firm specializing in automating acquisitions of high yield properties. River most recently joined Pinterest on their growth engineering team. River hails from Northern California, and spent his early childhood living on Native American reservations.",
     picUrl: "/images/river.jpg",
     email: "river@rnjventures.com",
+  },
+  {
+    name: "Nat Redfern",
+    bio:
+      "Nat is a third generation technologist, and product manager at Disney+ running half a dozen products. Since joining, he’s built products that have delivered $60M in recurring revenue for Disney+. Prior to Disney, Nat worked as the Chief of Staff for Contrary Capital, where he managed a team of 100+ scouts and invested in early stage startups. ",
+    picUrl: "/images/nat.jpg",
+    email: "nat@rnjventures.com",
   },
 ];
 const Profiles = () => (
