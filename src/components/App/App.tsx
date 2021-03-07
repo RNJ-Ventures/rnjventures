f338d283a0c87256f80879f16066403abb3c3f99
JonLuca DeCaro <jdecaro@usc.edu>
2021-03-07 02:08:35 -0500

    profiles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
 import Profiles from "../ProfileCard/Profiles";
 
 const App = () => (
-  <BrowserRouter basename="/">
-    <div className="App">
-      <Route path="/profiles" component={Profiles} />
-    </div>
-  </BrowserRouter>
+  <div className="App">
+    <Profiles />
+  </div>
 );
 
 export default App;
