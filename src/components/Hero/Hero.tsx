8f3c24fa6f6ba7c2fe503b69d6feafeec5e2bded
RivMist <rivmist@gmail.com>
2021-03-07 12:06:17 -0500

    Hero maybe?

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import HeroBackgroundGrid from "./HeroBackgroundGrid";
 import "./Hero.css";
 
 export default function Hero() {
   return (
     <div className="Hero">
       <h2>We are a bunch of software fucks</h2>
+      <div className="HeroBackground">
+        <HeroBackgroundGrid
+          width={30}
+          height={40}
+          space={30}
+          radius={3}
+          fill="#fff"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Hero/HeroBackgroundGrid.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type Props = {
+  width: number;
+  height: number;
+  space: number;
+  radius: number;
+  fill: string;
+};
+
+export default ({ width, height, space, radius, fill }: Props) => {
+  const viewWidth = width * radius * 2 + (width - 2) * (space - radius * 2);
+  const viewHeight = height * radius * 2 + (height - 2) * (space - radius * 2);
+  const dots = [];
+
+  for (let indexWidth = 0; indexWidth < width; indexWidth += 1) {
+    for (let indexHeight = 0; indexHeight < height; indexHeight += 1) {
+      dots.push(
+        <circle
+          key={`${indexWidth}-${indexHeight}`}
+          cx={radius + indexWidth * space}
+          cy={radius + indexHeight * space}
+          r={radius * (1 + indexWidth / 25)}
+        />
+      );
+    }
+  }
+
+  return (
+    <svg
+      width={viewWidth}
+      height={viewHeight}
+      viewBox={`0 0 ${viewWidth} ${viewHeight}`}
+      version="1.1"
+    >
+      <g fill={fill}>{dots}</g>
+    </svg>
+  );
+};
