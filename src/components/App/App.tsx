4b5f7b8315a303bf0334196cdb6bde9096e5f9f4
RivMist <rivmist@gmail.com>
2021-03-07 11:41:06 -0500

    Setup hero

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Profiles from "../ProfileCard/Profiles";
 import Header from "./Header/Header";
+import Hero from "../Hero/Hero";
 
 const App = () => (
   <div className="App">
     <Header />
     <div className="Body">
+      <Hero />
       <Profiles />
     </div>
   </div>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="Hero">
+      <h2>We are a bunch of software fucks</h2>
+    </div>
+  );
+}
