8c329b4fe638ce326a398185c2cd489e0ff54634
JonLuca DeCaro <jdecaro@usc.edu>
2021-03-07 13:28:43 -0500

    mission

--- src/components/Mission/Mission.tsx
+++ src/components/Mission/Mission.tsx
@@ -5,11 +5,11 @@ const Mission = () => {
   return (
     <div className="Mission" id="mission">
       <h1>Mission</h1>
-      <span>
+      <h2>
         We believe in translating our silicon valley experience into domain
         specific knowledge that is a high value add to niche business with solid
         margins, a healthy customer base, and a solid technical foundation.
-      </span>
+      </h2>
     </div>
   );
 };
