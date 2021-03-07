681cb675b9276d4f6d35213593952970e3821530
JonLuca DeCaro <jdecaro@usc.edu>
2021-03-07 12:44:39 -0500

    css

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,15 +6,7 @@ export default function Hero() {
   return (
     <div className="Hero">
       <h2>We are a bunch of software fucks</h2>
-      <div className="HeroBackground">
-        <HeroBackgroundGrid
-          width={30}
-          height={40}
-          space={30}
-          radius={3}
-          fill="#fff"
-        />
-      </div>
+      <div className="HeroBackground" />
     </div>
   );
 }
